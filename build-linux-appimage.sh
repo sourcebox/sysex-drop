38cf797b72bded5b446fafecbad200baaaaeb1f5
Oliver Rockstedt <info@sourcebox.de>
2021-08-29 11:48:54 +0200

    Icons for AppImage

--- build-linux-appimage.sh
+++ build-linux-appimage.sh
@@ -7,7 +7,12 @@ rm -rf target/release/appimage/*
 linuxdeploy-x86_64.AppImage \
     --executable ./target/release/sysex-drop \
     --desktop-file ./tools/sysex-drop.desktop \
-    --icon-file ./tools/sysex-drop.png \
+    --icon-file ./icons/16x16/sysex-drop.png \
+    --icon-file ./icons/32x32/sysex-drop.png \
+    --icon-file ./icons/64x64/sysex-drop.png \
+    --icon-file ./icons/128x128/sysex-drop.png \
+    --icon-file ./icons/256x256/sysex-drop.png \
+    --icon-file ./icons/sysex-drop.svg \
     --appdir ./target/release/appimage/AppDir \
     --output appimage
 
